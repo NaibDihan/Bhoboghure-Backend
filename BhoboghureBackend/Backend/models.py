e6fa803b4adab18f45150faca418e0cb1f8a4c92
Naib Uddin <afmnaibuddin@gmail.com>
2023-12-02 17:23:16 +0600

    Committed

--- BhoboghureBackend/Backend/models.py
+++ BhoboghureBackend/Backend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone_no = models.CharField(max_length=11)
+
+    def __str_(self):
+        return self.name 
--- BhoboghureBackend/Backend/views.py
+++ BhoboghureBackend/Backend/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from  django.http import JsonResponse
 
 # Create your views here.
+from .models import Customer
+from .forms import CustomerForm
+
+
+
+def index(request):
+    customers_list = Customer.objects.all()
+    context = {'customers_list': customers_list}
+    return render(request,'/home/index.html',context=context)
